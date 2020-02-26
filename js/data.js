29f5874fe6c87c3958007a761ecbb22fb25be577
forsmarts <forsmarts@gmail.com>
2020-02-26 13:22:24 +0300

    New levels and images

--- js/data.js
+++ js/data.js
@@ -63,6 +63,18 @@ storage.addPuzzle({
 
 storage.addPuzzle({
   "gridSize": [4,4],
+  "goal": [6,6],
+  "hint": "The goal of the level can contain few numbers - you should have corresponding amount of them in the grid",
+  "numbers": [
+    [1,2,2, ],
+    [2,3,3,2],
+    [1,2,3,2],
+    [ , ,2,1]
+  ]
+});
+
+storage.addPuzzle({
+  "gridSize": [4,4],
   "goal": [9],
   "numbers": [
     [ ,1,2,3],
@@ -84,14 +96,36 @@ storage.addPuzzle({
 });
 
 storage.addPuzzle({
+  "gridSize": [5,3],
+  "goal": [8],
+  "hint": "Modifying tiles will change the number on the ajdacent tile accordingly, if they're moved towards it",
+  "numbers": [
+    [2,3,4,2,1],
+    [1,2,4,"+1", ],
+    ["+1",1,2,1, ]
+  ]
+});
+
+storage.addPuzzle({
   "gridSize": [5,4],
   "goal": [8,8],
-  "hint": "The goal of the level can contain few numbers - you should have corresponding amount of them in the grid",
   "numbers": [
     [1,4,4,6,5],
     [2,5,3,2,2],
     [3,4,2,1,1],
     [2,3,2, , ]
+  ]
+});
+
+storage.addPuzzle({
+  "gridSize": [5,5],
+  "goal": [6,8],
+  "numbers": [
+    [ , ,2,2,1],
+    [2,"+1",3,2,1],
+    [3,4,"-1",2, ],
+    [5,4,2,1, ],
+    [3,3,2, , ]
   ]
 });
 
